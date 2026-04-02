86e65fdff61c39db05c573194ca7e6bbbccc5e96
JecielAncuna <jecielancuna@gmail.com>
2026-04-02 17:32:29 +0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+require("dotenv").config();
+const express = require("express");
+const { connectToMongoDB } = require("./database");
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'dist')));  // ← changed
+
+const router = require("./routes");
+app.use("/api", router);  // ← moved up before catch-all
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
+  
+const port = process.env.PORT || 5000;
+
+const startServer = async () => {
+    await connectToMongoDB();
+    app.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`);
+    });
+};
+startServer();
