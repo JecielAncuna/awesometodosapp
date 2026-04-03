dfeb6ee80325428b0cbf62d26e49449863f17b2c
JecielAncuna <jecielancuna@gmail.com>
2026-04-03 19:21:16 +0800

    initial commit

--- server/routes.js
+++ server/routes.js
@@ -14,7 +14,13 @@ router.get("/todos", async (req, res) => {
 /// POST /todos
 router.post("/todos", async (req, res) => {
   const collection = getCollection();
+<<<<<<< HEAD
   const { todo } = req.body;
+=======
+  let { todo } = req.body;
+
+  todo = JSON.stringify(todo);
+>>>>>>> ef4fc11 (initial commit)
 
   const newTodo = await collection.insertOne({ todo, status: false });
   res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
